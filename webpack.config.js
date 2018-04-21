2d00ee43ee512147a4f27e4eeaba79866999799d
Chen Wei <wei.chen@cobinhood.com>
2018-04-21 18:14:21 +0800

    only analyze bundle for production

--- webpack.config.js
+++ webpack.config.js
@@ -2,54 +2,65 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
-    entry: {
-        app: './app/app.ts',
-    },
-    output: {
-        filename: '[name].[chunkhash].js',
-        path: path.join(__dirname, './build'),
-        chunkFilename: '[name].[chunkhash].js',
-        publicPath: '/',
-    },
-    optimization: {
-        runtimeChunk: false,
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                  test: /[\\/]node_modules[\\/]/,
-                  name: 'vendor',
-                  chunks: 'all',
+
+module.exports = (env, options) => {
+
+    const plugins = [
+        new HtmlWebpackPlugin({
+            title: 'My Webpack Boilerplate',
+            template: 'index.html'
+          }),
+    ];
+
+    if (options.mode === 'production') {
+        plugins.push(
+            new BundleAnalyzerPlugin({
+                analyzerMode: 'static',
+                reportFilename: 'bundle_analyze.html',
+                openAnalyzer: false
+            })
+        );
+    }
+
+    return {
+        entry: {
+            app: './app/app.ts',
+        },
+        output: {
+            filename: '[name].[chunkhash].js',
+            path: path.join(__dirname, './build'),
+            chunkFilename: '[name].[chunkhash].js',
+            publicPath: '/',
+        },
+        optimization: {
+            runtimeChunk: false,
+            splitChunks: {
+                cacheGroups: {
+                    commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendor',
+                    chunks: 'all',
+                    }
                 }
-              }
-        }
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'My Webpack Boilerplate',
-        template: 'index.html'
-      }),
-      new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
-        reportFilename: 'bundle_analyze.html',
-        openAnalyzer: false
-      })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ]
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
+            }
+        },
+        plugins,
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.tsx?$/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ]
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"]
+        },
+    }
 };
