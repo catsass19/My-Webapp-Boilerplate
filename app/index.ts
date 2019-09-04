213632d98fd0eddc28f300e77c289eaa4ccd40c2
Chen Wei <wei.chen@cobinhood.com>
2019-09-04 20:09:04 +0800

    do not register service work in dev mode

--- app/index.ts
+++ app/index.ts
@@ -10,19 +10,19 @@ import appService from '@/service/app';
     } catch (err) {
       console.error('Failed to inject debug service', err);
     }
+  } else {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker
+          .register('/service-worker.js')
+          .then(registration => {
+            console.log('SW registered: ', registration);
+          })
+          .catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+          });
+      });
+    }
   }
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register('/service-worker.js')
-        .then(registration => {
-          console.log('SW registered: ', registration);
-        })
-        .catch(registrationError => {
-          console.log('SW registration failed: ', registrationError);
-        });
-    });
-  }
-
   ReactDOM.render(AppMain, document.getElementById('content'));
 })();
