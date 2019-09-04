c3390eb4de0c65fb441ea5318b8e2f665e0fa83a
Chen Wei <wei.chen@cobinhood.com>
2019-09-04 19:51:48 +0800

    test husky precommit

--- app/index.ts
+++ app/index.ts
@@ -12,14 +12,17 @@ import appService from '@/service/app';
     }
   }
   if ('serviceWorker' in navigator) {
-       window.addEventListener('load', () => {
-         navigator.serviceWorker.register('/service-worker.js').then(registration => {
-         console.log('SW registered: ', registration);
-         }).catch(registrationError => {
-           console.log('SW registration failed: ', registrationError);
-         });
-       });
-     }
+      window.addEventListener('load', () => {
+       navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(registration => {
+          console.log('SW registered: ', registration);
+        })
+        .catch(registrationError => {
+          console.log('SW registration failed: ', registrationError);
+        });
+    });
+  }
 
   ReactDOM.render(AppMain, document.getElementById('content'));
 })();
